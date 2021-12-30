2faa6f34f5334a026f680d5a704b6c9f68ee49b4
smsakawat <sakawat602872@gmail.com>
2021-12-30 21:04:41 +0600

    updated

--- App.js
+++ App.js
@@ -1,37 +1,38 @@
-import React from 'react';
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-
-import { BookDetail } from "./screens/";
+import React from "react";
 import Tabs from "./navigation/tabs";
+import { BookDetail } from "./screens/";
 
 const theme = {
-    ...DefaultTheme,
-    colors: {
-        ...DefaultTheme.colors,
-        border: "transparent"
-    }
-}
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border: "transparent",
+  },
+};
 
 const Stack = createStackNavigator();
 
 const App = () => {
-    return (
-        <NavigationContainer theme={theme}>
-            <Stack.Navigator
-                screenOptions={{
-                    headerShown: false
-                }}
-                initialRouteName={'Home'}
-            >
-                {/* Tabs */}
-                <Stack.Screen name="Home" component={Tabs} />
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName={"Home"}
+      >
+        <Stack.Screen name="Home" component={Tabs} />
 
-                {/* Screens */}
-                <Stack.Screen name="BookDetail" component={BookDetail} options={{ headerShown: false }} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+        <Stack.Screen
+          name="BookDetail"
+          component={BookDetail}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
